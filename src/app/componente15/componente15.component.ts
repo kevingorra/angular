1c18ac255bb1caeb623ff94e81e2bcc5ee80ad76
CristianP50 <cristo>
2022-09-12 11:51:47 -0500

    inicio con angular

---
+++ src/app/componente15/componente15.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente15',
+  templateUrl: './componente15.component.html',
+  styleUrls: ['./componente15.component.css']
+})
+export class Componente15Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/componente16/componente16.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente16',
+  templateUrl: './componente16.component.html',
+  styleUrls: ['./componente16.component.css']
+})
+export class Componente16Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
